fc93b152f9b43faeb3da615aee7540dbeba044ab
HomaX1 <baitasova.anastasiia@gmail.com>
2017-02-22 23:22:57 +0200

    slide-show

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-$('.list__item').click(function () {
 
-        if (!$(this).find('.inside-list').is(':empty')) {
-            $(this).toggleClass('list__item_active');
-            $(this).find('.inside-list').slideToggle('slow');
+
+
+$(document).ready(function(){
+    $('.list__item').click(function () {
+
+            if (!$(this).find('.inside-list').is(':empty')) {
+                $(this).toggleClass('list__item_active');
+                $(this).find('.inside-list').slideToggle('slow');
+            }
+
         }
+    );
 
-    }
-);
+    $('.my-slide').slick({
+        dots: true,
+        infinite: true
+    });
+});
